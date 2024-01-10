48e874ae06177df5c752b6d2e567ac3715c490f5
blue <midnight@nightmarish.blue>
2024-01-10 20:45:30 +0000

    Added static stuff for Util.

--- src/main/java/blue/nightmarish/milked/Util.java
+++ src/main/java/blue/nightmarish/milked/Util.java
@@ -1,5 +1,15 @@
 package blue.nightmarish.milked;
 
+import net.minecraft.core.particles.ParticleOptions;
+import net.minecraft.util.Mth;
+import net.minecraft.world.level.Level;
+
 public class Util {
     public static final int COW_EAT_ANIMATION_TICKS = 40;
+    public static final int DROPLET_TICK_COOLDOWN = 10;
+
+    public static void spawnFluidParticle(Level pLevel, double pStartX, double pEndX, double pStartZ, double pEndZ, double pPosY, ParticleOptions pParticleOption) {
+        if (!pLevel.isClientSide) return;
+        pLevel.addParticle(pParticleOption, Mth.lerp(pLevel.random.nextDouble(), pStartX, pEndX), pPosY, Mth.lerp(pLevel.random.nextDouble(), pStartZ, pEndZ), 0.0D, 0.0D, 0.0D);
+    }
 }
